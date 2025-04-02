12a271d3059fbb8fb542d74d713f9599c833ac41
DattiVishnuVandana <vandanadatti567@gmail.com>
2025-04-02 18:42:56 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express=require('express')
 
 const app=express()
-
+//order of routers matters here ..keep / route at last ..if anything matches /..it handles
+app.use("/test/2",(req,res)=>{
+    res.send("abraka dabra")
+})
 app.use("/test",(req,res)=>{
     res.send("hello")
-})
+})//here /test/anything..handles but not /test345
+
 app.use("/",(req,res)=>{
     res.send("hello this home")
 })
