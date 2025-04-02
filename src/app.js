f919a5a35115a45bd73ed0ad4d66ff2a132ca22e
DattiVishnuVandana <vandanadatti567@gmail.com>
2025-04-02 18:53:52 +0530

    exploring use,get ,post etc

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express=require('express')
 
 const app=express()
-//order of routers matters here ..keep / route at last ..if anything matches /..it handles
+//order of routers matters here ..keep / route at last,(use also) ..if anything matches /..it handles
+app.get("/test/2",(req,res)=>{
+    res.send("abraka dabra...it only matches get call")
+})
 app.use("/test/2",(req,res)=>{
     res.send("abraka dabra")
 })
+
+//use-matches all http methods
 app.use("/test",(req,res)=>{
     res.send("hello")
 })//here /test/anything..handles but not /test345
